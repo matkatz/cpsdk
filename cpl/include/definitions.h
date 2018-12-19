3d829cc5e84537aa40924677dff9fcb86ae1672a
Katz, Matan <matan.katz@intel.com>
2018-12-19 13:35:08 +0200

    initial commit

---
+++ cpl/include/definitions.h
@@ -0,0 +1,5 @@
+#ifdef WIN32 
+#define  DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
---
+++ examples/basic_example/basic_example.cpp
@@ -0,0 +1,8 @@
+#include "cpl.h"
+
+int main()
+{
+    cpl::core c;
+    c.say_hello();
+    return 0;
+}
